fe1860359acc45166aff57e5180a7485c29cb909
Dustin Vu <dqvu0320@gmail.com>
2022-11-30 14:41:39 -0600

    first commit

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'comment',
+  titile: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy'
+    },
+    {
+      name: 'comment',
+      titile: 'Comment',
+      type: 'string'
+    }
+  ]
+}
---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  titile: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'User Name',
+      type: 'string'
+    },
+    {
+      name: 'image',
+      titile: 'Image',
+      type: 'string'
+    }
+  ]
+}
