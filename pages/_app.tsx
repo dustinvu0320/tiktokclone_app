1acbacfa0f5426c67a9b64c5419e3c50f95862ff
Dustin Vu <dqvu0320@gmail.com>
2022-11-30 18:37:51 -0600

    worked on Navbar & Sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
 
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
 export default function App({ Component, pageProps }: AppProps) {
   const [isSSR, setIsSSR] = useState(true);
 
@@ -13,11 +16,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <div>
-      Navbar
+      <Navbar />
       <div className="flex gap-6 md:gap-20">
         {/* Side bar */}
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          Sidebar
+          <Sidebar />
         </div>
         {/* Main Content */}
         <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
