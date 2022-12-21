71c6fbd4bb3c7e8d5bc03c271835b0e763f0337e
Dustin Vu <dustinvu0320@gmail.com>
2022-12-21 00:20:22 -0600

    worked on SuggestedAccount and Comments part

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,11 +2,21 @@ import create from "zustand"
 import {persist} from "zustand/middleware"
 import axios from "axios"
 
+import { BASE_URL } from "../utils"
+
 const authStore = (set : any) => ({
   userProfile: null,
+  allUsers: [],
+
 // This addUser function helps to pass in parameter user and set userProfile state to be user
   addUser: (user: any) => set({userProfile: user}),
   removeUser: () => set({userProfile: null}),
+
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+
+    set({allUsers: response.data});
+  }
 })
 
 const useAuthStore = create (
